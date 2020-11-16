7cfbfcb8f91dc4f52195d33fb22b788f95892b9a
MannerlyMeat <ut.eugene@github.com>
2020-11-16 14:27:14 +0300

    Основа

--- script.py
+++ script.py
@@ -9,12 +9,24 @@ import json
 import numpy as np
 import getpass
 import os.path
+from random import randint
 
+timer = 0
+#global groups
 
 def post_script():
 
     #выгрузка id групп
+    global groups
+    global vk_api
+    global text
+    global i
+    global owner
+    global photo_id
+    global post_link
+    
     groups = np.loadtxt("groups.txt", delimiter='\t', dtype=np.int)
+    #print(groups)
     #подсчет элементов в массиве
     try:
         temp = len(groups)
@@ -64,7 +76,7 @@ def post_script():
     else:
         link_yn = 0
         
-    #цикл постинга    
+    #цикл постинга
     for i in range(temp):
         text = open(textf + '.txt')
         #постинг в группу по порядковому номеру в массиве + получение данных о сервре для загрузки фото и её загрузка
@@ -88,44 +100,19 @@ def post_script():
         gr_pos = abs(groups[i])
         try:
             if img_yn == 1 and link_yn == 0:
-                vk_api.wall.post(message=' '.join(map(str,text)), owner_id=groups[i], attachments="photo"+owner+"_"+photo_id)
-                post_id = vk_api.wall.get(owner_id = groups[1], count = 1)
-                post_id = post_id['items']
-                post_id = post_id[0]
-                post_id = post_id['id']
-                with open("post_address.txt", "a") as file:
-                    file.write("vk.com/wall" + str(groups[i]) + "_" + str(post_id)+ "\n")
-                time.sleep(5)
+                text_and_image()             
             elif img_yn == 0 and link_yn == 0:
-                vk_api.wall.post(message=' '.join(map(str,text)), owner_id=groups[i])
-                post_id = vk_api.wall.get(owner_id = groups[1], count = 1)
-                post_id = post_id['items']
-                post_id = post_id[0]
-                post_id = post_id['id']
-                with open("post_address.txt", "a") as file:
-                    file.write("vk.com/wall" + str(groups[i]) + "_" + str(post_id)+ "\n")
-                time.sleep(5)
+                only_text()
             elif img_yn == 0 and link_yn == 1:
-                vk_api.wall.post(message=' '.join(map(str,text)) + "\n Ссылка - " + post_link, owner_id=groups[i])
-                post_id = vk_api.wall.get(owner_id = groups[1], count = 1)
-                post_id = post_id['items']
-                post_id = post_id[0]
-                post_id = post_id['id']
-                with open("post_address.txt", "a") as file:
-                    file.write("vk.com/wall" + str(groups[i]) + "_" + str(post_id)+ "\n")
-                time.sleep(5)
+                text_and_link()
             elif img_yn == 1 and link_yn == 1:
-                vk_api.wall.post(message=' '.join(map(str,text)) + "\n Ссылка - " + post_link, owner_id=groups[i], attachments="photo"+owner+"_" + photo_id)
-                post_id = vk_api.wall.get(owner_id = groups[1], count = 1)
-                post_id = post_id['items']
-                post_id = post_id[0]
-                post_id = post_id['id']
-                with open("post_address.txt", "a") as file:
-                    file.write("vk.com/wall" + str(groups[i]) + "_" + str(post_id)+ "\n")
-                time.sleep(5)
+                image_and_link()
+
             print('Успешно')
-            
+
             p+=1
+            if p == 50:
+                break
         except vk.exceptions.VkAPIError as er:
             print('Стена закрыта или другая ошибка')
             print(er)
@@ -133,4 +120,77 @@ def post_script():
     print("Успешно сделаных записей - " + str(p))
     input("Нажмите Enter для продлжения")
 
+
+def random_time():
+    global timer
+    timer = randint(20,40)
+
+def delete_from_list():
+    f = open("groups.txt")
+    output = []
+    for line in f:
+        if not str(groups[i]) in line:
+            output.append(line)
+    f.close()
+    f = open("groups.txt", "w")
+    f.writelines(output)
+    f.close()
+
+
+def only_text():
+    vk_api.wall.post(message=' '.join(map(str,text)), owner_id=groups[i])
+    post_id = vk_api.wall.get(owner_id = groups[i], count = 1)
+    post_id = post_id['items']
+    post_id = post_id[0]
+    post_id = post_id['id']
+    with open("post_address.txt", "a") as file:
+        file.write("vk.com/wall" + str(groups[i]) + "_" + str(post_id)+ "\n")
+    delete_from_list()
+    random_time()
+    print(timer)
+    time.sleep(int(timer))
+
+
+def text_and_image():
+    vk_api.wall.post(message=' '.join(map(str,text)), owner_id=groups[i], attachments="photo"+owner+"_"+photo_id)
+    post_id = vk_api.wall.get(owner_id = groups[i], count = 1)
+    post_id = post_id['items']
+    post_id = post_id[0]
+    post_id = post_id['id']
+    with open("post_address.txt", "a") as file:
+        file.write("vk.com/wall" + str(groups[i]) + "_" + str(post_id)+ "\n")
+    delete_from_list()
+    random_time()
+    print(timer)
+    time.sleep(int(timer)) 
+
+
+def text_and_link():
+    vk_api.wall.post(message=' '.join(map(str,text)) + "\n Ссылка - " + post_link, owner_id=groups[i])
+    post_id = vk_api.wall.get(owner_id = groups[1], count = 1)
+    post_id = post_id['items']
+    post_id = post_id[0]
+    post_id = post_id['id']
+    with open("post_address.txt", "a") as file:
+        file.write("vk.com/wall" + str(groups[i]) + "_" + str(post_id)+ "\n")
+    delete_from_list()
+    random_time()
+    print(timer)
+    time.sleep(int(timer))
+
+
+def image_and_link():
+    vk_api.wall.post(message=' '.join(map(str,text)) + "\n Ссылка - " + post_link, owner_id=groups[i], attachments="photo"+owner+"_" + photo_id)
+    post_id = vk_api.wall.get(owner_id = groups[1], count = 1)
+    post_id = post_id['items']
+    post_id = post_id[0]
+    post_id = post_id['id']
+    with open("post_address.txt", "a") as file:
+        file.write("vk.com/wall" + str(groups[i]) + "_" + str(post_id)+ "\n")
+    delete_from_list()
+    random_time()
+    print(timer)
+    time.sleep(int(timer))    
+    
 post_script()
+#random_time()
